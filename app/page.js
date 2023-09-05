38f481637671ee41d37baab9302050145aa41f11
Jarrod <jarrmill@outlook.com>
2023-09-05 11:16:02 -0400

    Add threejs, related dependencies and scrolling computers on background

--- app/page.js
+++ app/page.js
@@ -1,18 +1,15 @@
-import Image from 'next/image'
-import Link from 'next/link';
+"use client";
+
+import {Suspense, useState } from 'react'
 import styles from './page.module.css'
+import Computers from './Computers'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <h1>
-          <span className={styles.title}>Jarrod</span>
-          <span className={styles.secondary_title}>Codes</span>
-        </h1>
-        <Image src="/construction.gif" width={250} height={250} />
-        <Link
-          href={`/travel/`}
-          >Click to go to Jarrod Travels</Link>
+        <Suspense fallback={null}>
+          <Computers />
+        </Suspense>
     </main>
   )
 }
